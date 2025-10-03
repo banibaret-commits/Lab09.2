21128ee0bb40ff9e3174373f1ecde9de2347e632
aya <nellymtl4@gmail.com>
2025-10-03 11:49:59 -0400

    New button Pink!

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -6,6 +8,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
+        Button but = new Button("Pink!");
         
     }
 }
